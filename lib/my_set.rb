0d84050650ae2d8976c70647846062c07d611d4d
Eli Craig <elidcraig@gmail.com>
2022-07-03 13:50:12 -0700

    commit tree 44fb5a43d64f0b744177daa65ae473117e5edc9f

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,39 @@
-# your code here
+class MySet
+  def initialize value=[]
+    @hash = {}
+    value.each {|item| @hash[item] = true}
+  end
+
+  def include? value
+    @hash[value] ? true : false
+  end
+
+  def add value
+    @hash[value] = true
+    return self
+  end
+
+  def delete value
+    @hash.delete value
+    return self
+  end
+
+  def size
+    @hash.length
+  end
+
+  def clear
+    @hash = {}
+    return self
+  end
+
+  def inspect
+    nums = @hash.keys
+    "#<MySet: #{...nums}>"
+  end
+
+  # def each
+  #   @hash.each
+  #   return self
+  # end
+end
